7222ca4f317d40a59d80887dbfafd004429e2ea7
Martin <gexor@zelx.de>
2023-09-22 16:58:26 +0200

    Update aa_buchungenBuffer.php
    php 7

--- Website/aa_buchungenBuffer.php
+++ Website/aa_buchungenBuffer.php
@@ -5,7 +5,10 @@ $sql1 = "SELECT id, teamID, verwendungszweck, betrag FROM ".$prefix."buchungenBu
 $sql2 = mysql_query($sql1);
 while ($sql3 = mysql_fetch_assoc($sql2)) {
 	$loeschen1 = "DELETE FROM ".$prefix."buchungenBuffer WHERE id = ".$sql3['id'];
-	$loeschen2 = mysql_query($loeschen1);
+$loeschen2 = mysqli_query($mysqli, $loeschen1);
+if (mysqli_error($mysqli)) {
+	echo "Fehler beim Löschen der Buchung: " . mysqli_error($mysqli);
+}
 	if (mysql_affected_rows() > 0) {
 		$buch1 = "INSERT INTO ".$prefix."buchungen (team, verwendungszweck, betrag, zeit) VALUES ('".$sql3['teamID']."', '".$sql3['verwendungszweck']."', ".$sql3['betrag'].", ".time().")";
 		$buch2 = mysql_query($buch1);
@@ -16,8 +19,11 @@ while ($sql3 = mysql_fetch_assoc($sql2)) {
 $sql1 = "SELECT id, teamID, pointsGained FROM ".$prefix."eloBuffer WHERE ausfuehren < ".time()." LIMIT 0, 80";
 $sql2 = mysql_query($sql1);
 while ($sql3 = mysql_fetch_assoc($sql2)) {
-	$loeschen1 = "DELETE FROM ".$prefix."eloBuffer WHERE id = ".$sql3['id'];
-	$loeschen2 = mysql_query($loeschen1);
+	$loeschen1 = "DELETE FROM ".$prefix."buchungenBuffer WHERE id = ".$sql3['id'];
+$loeschen2 = mysqli_query($mysqli, $loeschen1);
+if (mysqli_error($mysqli)) {
+	echo "Fehler beim Löschen der Buchung: " . mysqli_error($mysqli);
+}
 	if (mysql_affected_rows() > 0) {
 		$eloChange1 = "UPDATE ".$prefix."teams SET elo = elo+".$sql3['pointsGained']." WHERE ids = '".$sql3['teamID']."'";
 		$eloChange2 = mysql_query($eloChange1);
